913220263586535d854527f75cb0d0b14de83511
grzegorz-sobocinski-wsei <48628417+grzegorz-sobocinski-wsei@users.noreply.github.com>
2019-03-16 19:02:45 +0100

    Created VS solution

---
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GraMonolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj");
+        }
+
+
+
+    }
+}
